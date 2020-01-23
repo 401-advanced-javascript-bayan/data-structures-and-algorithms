31fafb02310a28eff99c273c6c84fcb68c35304e
bayan <bayana.mhmd99@gmail.com>
2020-01-23 05:12:51 +0200

    still need test

--- array-shift/array-shift.js
+++ array-shift/array-shift.js
@@ -1,4 +1,14 @@
 'use strict';
-function BinarySearch(arr,value){
-    let oper = Math.floor(arr.length / 2);
+function insertShiftArray(arr,value){
+    let middleArray = Math.floor(arr.length / 2);    // we used math.floor cuz we can have random nambers
+        let newArray = [];
+    
+        for (let i = 0; i < arr.length; ++i) {
+            if (i < middleArray){newArray[i] = arr[i]; }
+            if (i == middleArray){newArray[i] = value;}    
+            if (i > middleArray){newArray[i] = arr[i - 1];}
+        }
+        return newArray;
     }
+    console.log(insertShiftArray);
+    module.exports = insertShiftArray;
