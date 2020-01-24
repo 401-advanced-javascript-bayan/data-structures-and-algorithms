d040e174c2cf8541e6517e529f5515268bb19573
bayanmhmd <55940862+bayanmhmd@users.noreply.github.com>
2020-01-25 00:25:05 +0200

    Update array-shift.js

--- array-shift/array-shift.js
+++ array-shift/array-shift.js
@@ -1,9 +1,1 @@
-'use strict';
-const reverseArray = (arr) => {
-    let newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        newArr[arr.length - i - 1] = arr[i];
-    }
-    console.log(newArr);
-    return newArr;
-}
+
