aff1168c4c74dae1132662759783dbacd45ab0e0
bayan <bayana.mhmd99@gmail.com>
2020-01-26 20:50:19 +0200

    missing

---
+++ linked.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const Node = require('../challenge/node.js');
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0
+    }
+
+        /////////////////////// create new node /////////////////////////
+    insert(value) {
+        let node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+            this.length++
+             
+            return  this;
+        }else{
+        
+        let currentNode = this.head;
+
+        while (currentNode.next) {
+            currentNode = currentNode.next;
+        }  
+        currentNode.next = node;
+        this.length++ ;
+
+               return this;
+      }
+    }
+     /////////// check on the list ///////////
+    
+    includes(value) {
+        
+        
+        
+        let currentNode = this.head;
+        while (currentNode) {
+            if (currentNode.value === value) {  // if my value so go to next node
+                currentNode = currentNode.next;
+                return true;
+            }
+            return false;   
+        }
+        
+    }  
+    
+
+   
+}
+
+
+
+module.exports=  LinkedList 
+   
