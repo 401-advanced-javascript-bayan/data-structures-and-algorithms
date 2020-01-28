e85011cf7652841cea39baadc6516c15cb7248db
bayan <bayana.mhmd99@gmail.com>
2020-01-28 21:31:12 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # data-structures-and-algorithms
+
+
+Singly Linked List
+
+Short brief how it's work and linked to each other as a object chain
+Challenge
+
+Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge.
+Approach & Efficiency
+used class and constructor format , while loop
+
+API
+
+insert : append new node into linked list
+includes : find certain node
+to string : convert all list items to string
+
+![]()
--- challenge/lib/linked.js
+++ challenge/lib/linked.js
@@ -1,43 +1,85 @@
 'use strict';
 
-
+class Node {
+    constructor(value) {
+        this.value = value;      // node                           //chear and easy ??!!
+        this.next = null;
+    }
+}
 
 class LinkedList {
-  constructor() {
-    this.head = null;   /// in first case if there is nothing its null
-  }
- /////////// create node////////////////
-  insert(value) {
-    let node = new Node(value);
-
-
-
-    if (!this.head) {            
-      this.head = node;
+    constructor() {                   /// in first case if there is nothing its null
+        this.head = null
     }
-    
-    let currentNode = this.head;
-    while(currentNode.next) {
-      currentNode = currentNode.next
+////////////////////////////////////////// create node////////////////////////
+    insert(value) {
+
+        let node = new Node(value);
+
+        if (!this.head) {
+            this.head = node
+            return this
+        }
+
+        let currentNode = this.head;
+        while (currentNode.next) {
+            currentNode = currentNode.next
+        }
+
+        currentNode.next = node;
+        return this
     }
-
-    currentNode.next = node;
-    return this;
-  }
-  ////////////////////////// check on the list ///////////////////////
-
-  includes(value){
-      let currentNode = this.head ;
-      while (currentNode.next) {
-     if(currentNode.next.val === value){
-              return true ;
-      }
-      currentNode = currentNode.next ;
-      }
-      return false ;
+/////////////////////////////////////// check on the list ///////////////////////
+    includes(value) {
+        let currentNode = this.head
+        while (currentNode) {
+            if (currentNode.value === value) {
+                return true
+            } else {
+                currentNode = currentNode.next
+            }
+        }
+        return false
     }
-//////////////////////////string /////////////////////////
-   
+////////////////////////////////// string /////////////////////////////////////
+    toString() {
+        let currentNode = this.head;           
+        let string = ''
+        while (currentNode) {                         // currentNode is head
+           string += ` ${currentNode.value} ->`
+            currentNode = currentNode.next;                 
+        }
+        return string += ` NULL`
+    }
+}
 
 
- module.exports = LinkedList;
+module.exports = LinkedList
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
