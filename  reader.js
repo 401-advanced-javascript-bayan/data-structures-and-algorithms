71cae647c6a22abd7ca779740d2dd7c0bee92e83
bayan <bayana.mhmd99@gmail.com>
2020-01-29 07:45:20 +0200

    ok

---
+++  reader.js
@@ -0,0 +1,12 @@
+},
+"repository": {
+  "type": "git",
+  "url": "git+https://github.com/401-advanced-javascript-ibrahim/data-structures-amd-algorithms.git"
+},
+"author": "",
+"license": "ISC",
+"bugs": {
+  "url": "https://github.com/401-advanced-javascript-ibrahim/data-structures-amd-algorithms/issues"
+},
+"homepage": "https://github.com/401-advanced-javascript-ibrahim/data-structures-amd-algorithms#readme"
+}
