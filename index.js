cf08a4f1641c22daf941824b71ae18bdea4699c2
bayan <bayana.mhmd99@gmail.com>
2020-01-29 07:41:26 +0200

    ok

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-/////////////////////
+///////////////////// challenge ////////
+'use strict';
+const  challenge = require('../lib/interview.js');
+
+describe('the first function in interview', () => {
+    describe('this will test sumtion metrix', () => {
+
+        it('Test the internal arrys in the main array', () => {
+            const arr = [[1, 2, 3], [3, 5, 7], [1, 7, 10]]
+            expect(challenge.sumtionMatrix(arr)).toEqual([6, 15, 18])
+        })
+    })
+})
