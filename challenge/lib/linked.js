5eb6c621a02a799c48141fa01d0aabf23603762a
bayan <bayana.mhmd99@gmail.com>
2020-01-29 19:02:17 +0200

    ok

--- challenge/lib/linked.js
+++ challenge/lib/linked.js
@@ -1,60 +1,60 @@
 'use strict';
 
 class Node {
-    constructor(value) {
-        this.value = value;      // node                           //chear and easy ??!!
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;      // node                           //chear and easy ??!!
+    this.next = null;
+  }
 }
 
 class LinkedList {
-    constructor() {                   /// in first case if there is nothing its null
-        this.head = null
-    }
-////////////////////////////////////////// create node////////////////////////
-    insert(value) {
+  constructor() {                   /// in first case if there is nothing its null
+    this.head = null;
+  }
+  ////////////////////////////////////////// create node////////////////////////
+  insert(value) {
 
-        let node = new Node(value);
+    let node = new Node(value);
 
-        if (!this.head) {
-            this.head = node
-            return this
-        }
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
 
-        let currentNode = this.head;
-        while (currentNode.next) {
-            currentNode = currentNode.next
-        }
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
 
-        currentNode.next = node;
-        return this
+    currentNode.next = node;
+    return this;
+  }
+  /////////////////////////////////////// check on the list ///////////////////////
+  includes(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
     }
-/////////////////////////////////////// check on the list ///////////////////////
-    includes(value) {
-        let currentNode = this.head
-        while (currentNode) {
-            if (currentNode.value === value) {
-                return true
-            } else {
-                currentNode = currentNode.next
-            }
-        }
-        return false
+    return false;
+  }
+  ////////////////////////////////// string /////////////////////////////////////
+  toString() {
+    let currentNode = this.head;           
+    let string = '';
+    while (currentNode) {                         // currentNode is head
+      string += ` ${currentNode.value} ->`;
+      currentNode = currentNode.next;                 
     }
-////////////////////////////////// string /////////////////////////////////////
-    toString() {
-        let currentNode = this.head;           
-        let string = ''
-        while (currentNode) {                         // currentNode is head
-           string += ` ${currentNode.value} ->`
-            currentNode = currentNode.next;                 
-        }
-        return string += ` NULL`
-    }
+    return string += ` NULL`;
+  }
 }
 
 
-module.exports = LinkedList
+module.exports = LinkedList;
  
  
  
